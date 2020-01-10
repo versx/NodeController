adcebcde46bad241f7515cc4e04a2e6103ef63df
versx <versefx@gmail.com>
2020-01-09 18:16:17 -0800

    Redis tests

--- redis-client.js
+++ redis-client.js
@@ -4,16 +4,55 @@ const redis  = require('redis');
 const client = redis.createClient();
 
 client.on('error', function(err){
-    console.log('Something went wrong ', err)
+    console.log('Error occurred:', err)
 });
-client.set('my test key', 'my test value', redis.print);
-client.get('my test key', function(error, result) {
-    if (error) throw error;
-    console.log('GET result ->', result)
-});
-client.hset('HSET record', 'key', 'value', redis.print);
-client.hset('HSET record', 'second key', 'second value', redis.print);
 
 class RedisClient {
+    constructor() {
 
+    }
+    get(key) {
+        return client.get(key, function(err, result) {
+            if (err) throw err;
+            console.log("RedisClient::get:", result);
+        });
+    }
+    addPokemon(pokemon) {
+        var keys = Object.keys(pokemon);
+        keys.forEach(function(key) {
+            client.hset(pokemon.id, keys[key], redis.print);
+        });
+    }
+    addGym(gym) {
+        var keys = Object.keys(gym);
+        keys.forEach(function(key) {
+            client.hset(gym.id, keys[key], redis.print);
+        });
+    }
+    addRaid(raid) {
+        var keys = Object.keys(raid);
+        keys.forEach(function(key) {
+            client.hset(raid.id, keys[key], redis.print);
+        });
+    }
+    addPokestop(pokestop) {
+        var keys = Object.keys(pokestop);
+        keys.forEach(function(key) {
+            client.hset(pokestop.id, keys[key], redis.print);
+        });
+    }
+    addQuest(quest) {
+        var keys = Object.keys(quest);
+        keys.forEach(function(key) {
+            client.hset(quest.id, keys[key], redis.print);
+        });
+    }
+    addSpawnpoint(spawnpoint) {
+        var keys = Object.keys(spawnpoint);
+        keys.forEach(function(key) {
+            client.hset(spawnpoint.id, keys[key], redis.print);
+        });
+    }
 }
+
+module.exports = RedisClient;
