8c5fb291ec0d9b9297cf4a87cd2665f3d2b310fe
versx <versefx@gmail.com>
2020-01-12 01:38:43 -0800

    Add Assignment and AssignmentController structure

---
+++ src/controllers/assignment-controller.ts
@@ -0,0 +1,34 @@
+"use strict"
+
+import { Assignment } from "src/models/assignment";
+import { InstanceController } from "./instance-controller";
+
+class AssignmentController extends InstanceController {
+    constructor() {
+        super();
+    }
+    setup() {
+
+    }
+    addAssignment(assignment: Assignment) {
+
+    }
+    editAssignment(oldAssignment: Assignment, newAssignment: Assignment) {
+
+    }
+    deleteAssignment(assignment: Assignment) {
+
+    }
+    triggerAssignment(assignment: Assignment) {
+
+    }
+    instanceControllerDone(name: string) {
+
+    }
+}
+
+function todaySeconds() {
+
+}
+
+export { AssignmentController };
